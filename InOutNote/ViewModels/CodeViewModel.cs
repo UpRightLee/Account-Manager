cd38597916fed6223aa1dde07357b3ff5a7b15ab
JAMES-0813\haha5 <haha5ggoma@gmail.com>
2022-12-24 17:04:33 +0900

    2022-12-24 - 입출 내역, 코드, 자산 현황 화면 코드 작성, 각각의 ViewModel 1차 초안 작성 완료  Commit

--- InOutNote/ViewModels/CodeViewModel.cs
+++ InOutNote/ViewModels/CodeViewModel.cs
@@ -1,6 +1,8 @@
-﻿using InOutNote.Notifier;
+﻿using InOutNote.Command;
+using InOutNote.Notifier;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +11,174 @@ namespace InOutNote.ViewModels
 {
     public class CodeViewModel : INotifier
     {
+        private ObservableCollection<string> inOut = new ObservableCollection<string>();
+        private ObservableCollection<string> kind = new ObservableCollection<string>();
+        private ObservableCollection<string> bank = new ObservableCollection<string>();
+        private ObservableCollection<string> card = new ObservableCollection<string>();
+        private ObservableCollection<string> use = new ObservableCollection<string>();
+
+        private string selectedInOut = "";
+        private string selectedKind = "";
+        private string selectedBank = "";
+        private string selectedCard = "";
+        private string selectedUse = "";
+
+        public ObservableCollection<string> InOut
+        {
+            get { return inOut; }
+            set
+            {
+                inOut = value;
+                OnPropertyChanged("InOut");
+            }
+        }
+        public ObservableCollection<string> Kind
+        {
+            get { return kind; }
+            set
+            {
+                kind = value;
+                OnPropertyChanged("Kind");
+            }
+        }
+        public ObservableCollection<string> Card
+        {
+            get { return card; }
+            set
+            {
+                card = value;
+                OnPropertyChanged("Card");
+            }
+        }
+        public ObservableCollection<string> Bank
+        {
+            get { return bank; }
+            set
+            {
+                bank = value;
+                OnPropertyChanged("Bank");
+            }
+        }
+        public ObservableCollection<string> Use
+        {
+            get { return use; }
+            set
+            {
+                use = value;
+                OnPropertyChanged("Use");
+            }
+        }
+        public string SelectedInOut
+        {
+            get { return selectedInOut; }
+            set
+            {
+                selectedInOut = value;
+                OnPropertyChanged("SelectedInOut");
+            }
+        }
+        public string SelectedKind
+        {
+            get { return selectedKind; }
+            set
+            {
+                selectedKind = value;
+                OnPropertyChanged("SelectedKind");
+            }
+        }
+        public string SelectedCard
+        {
+            get { return selectedCard; }
+            set
+            {
+                selectedCard = value;
+                OnPropertyChanged("SelectedCard");
+            }
+        }
+        public string SelectedBank
+        {
+            get { return selectedBank; }
+            set
+            {
+                selectedBank = value;
+                OnPropertyChanged("SelectedBank");
+            }
+        }
+        public string SelectedUse
+        {
+            get { return selectedUse; }
+            set
+            {
+                selectedUse = value;
+                OnPropertyChanged("SelectedUse");
+            }
+        }
+        public RelayCommand LoadCodeViewCommand { get; }
+        public RelayCommand SelectDataCommand { get; }
+        public RelayCommand ExcelDownloadCommand { get; }
+        public RelayCommand AddDataCommand { get; }
+        public RelayCommand DeleteDataCommand { get; }
+        public CodeViewModel()
+        {
+            LoadCodeViewCommand = new RelayCommand(LoadCodeView);
+            SelectDataCommand = new RelayCommand(SelectData);
+            ExcelDownloadCommand = new RelayCommand(ExcelDownload);
+            AddDataCommand = new RelayCommand(AddData);
+            DeleteDataCommand = new RelayCommand(DeleteData);
+        }
+ 
+        private void DeleteData()
+        {
+            Console.WriteLine("Delete Data Command");
+        }
+
+        private void AddData()
+        {
+            Console.WriteLine("Add Data Command");
+        }
+
+        private void ExcelDownload()
+        {
+            Console.WriteLine("ExcelDownload Command");
+        }
+
+        private void SelectData()
+        {
+            Console.WriteLine("Select Data Command");
+        }
+
+        private void LoadCodeView()
+        {
+            InOut = new ObservableCollection<string>();
+            InOut.Add("입금");
+            InOut.Add("출금");
+            InOut.Add("전체");
+            SelectedInOut = "전체";
+
+            Kind = new ObservableCollection<string>();
+            Kind.Add("신용");
+            Kind.Add("체크");
+            Kind.Add("자동이체");
+            Kind.Add("전체");
+            SelectedKind = "전체";
+
+            Card = new ObservableCollection<string>();
+            Card.Add("IBK신용카드");
+            Card.Add("국민체크카드");
+            Card.Add("전체");
+            SelectedCard = "전체";
+
+            Bank = new ObservableCollection<string>();
+            Bank.Add("국민은행");
+            Bank.Add("기업은행");
+            Bank.Add("전체");
+            SelectedBank = "전체";
+
+            Use = new ObservableCollection<string>();
+            Use.Add("생활비");
+            Use.Add("가스비");
+            Use.Add("전체");
+            SelectedUse = "전체";
+        }
     }
 }
--- InOutNote/ViewModels/SummaryViewModel.cs
+++ InOutNote/ViewModels/SummaryViewModel.cs
@@ -1,6 +1,8 @@
-﻿using InOutNote.Notifier;
+﻿using InOutNote.Command;
+using InOutNote.Notifier;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +11,78 @@ namespace InOutNote.ViewModels
 {
     public class SummaryViewModel : INotifier
     {
+        private ObservableCollection<string> yearList = new ObservableCollection<string>();
+        private ObservableCollection<string> monthList = new ObservableCollection<string>();
+        
+        private string selectedYear = "";
+        private string selectedMonth = "";
+
+        public ObservableCollection<string> YearList
+        {
+            get { return yearList; }
+            set
+            {
+                yearList = value;
+                OnPropertyChanged("YearList");
+            }
+        }
+        public ObservableCollection<string> MonthList
+        {
+            get { return monthList; }
+            set
+            {
+                monthList = value;
+                OnPropertyChanged("MonthList");
+            }
+        }
+        public string SelectedYear
+        {
+            get { return selectedYear; }
+            set
+            {
+                selectedYear = value;
+                OnPropertyChanged("SelectedYear");
+            }
+        }
+        public string SelectedMonth
+        {
+            get { return selectedMonth; }
+            set
+            {
+                selectedMonth = value;
+                OnPropertyChanged("SelectedMonth");
+            }
+        }
+        public RelayCommand LoadSummaryViewCommand { get; }
+        public RelayCommand SelectDataCommand { get; }
+        public RelayCommand ExcelDownloadCommand { get; }
+        public SummaryViewModel() 
+        {
+            LoadSummaryViewCommand = new RelayCommand(LoadSummaryView);
+            SelectDataCommand = new RelayCommand(SelectData);
+            ExcelDownloadCommand = new RelayCommand(ExcelDownload);
+        }
+
+        private void ExcelDownload()
+        {
+            Console.WriteLine("ExcelDownload Command");
+        }
+        private void SelectData()
+        {
+            Console.WriteLine("Select Data Command");
+        }
+        private void LoadSummaryView()
+        {
+            YearList = new ObservableCollection<string>();
+            List<int> years = Enumerable.Range(1950, DateTime.Today.Year).ToList();
+
+            years.ForEach(year => YearList.Add(year.ToString()));
+            SelectedYear = DateTime.Today.Year.ToString();
+
+            MonthList = new ObservableCollection<string>();
+            List<int> months = Enumerable.Range(1, 12).ToList();
+            months.ForEach(month => MonthList.Add(month.ToString()));
+            SelectedMonth = "전체";
+        }
     }
 }
